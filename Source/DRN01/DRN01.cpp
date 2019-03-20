e24e2fcb61c41986fb3868f90f4973e53e440cf7
anthony malagutti <anthonymalagutti@gmail.com>
2019-03-19 21:45:19 -0600

    Project creation

---
+++ Source/DRN01/DRN01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DRN01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DRN01, "DRN01" );
